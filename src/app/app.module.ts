bd03dd47f3f39713e393fda9b374fc20da444eaa
Gabriel <gabriellagrota23@gmail.com>
2023-02-23 12:05:13 -0300

    feat: Criada lógica de iteração de itens na tabela

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -12,7 +13,8 @@ import { HomeModule } from './modules/home/home.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HomeModule
+    HomeModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/modules/home/components/todo-list/todo-list.component.ts
+++ src/app/modules/home/components/todo-list/todo-list.component.ts
@@ -1,10 +1,14 @@
+import { TaskList } from './../../model/task-list';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
-  styleUrls: ['./todo-list.component.scss']
+  styleUrls: ['./todo-list.component.scss'],
 })
 export class TodoListComponent {
-
+  public taskList: Array<TaskList> = [
+    { task: 'Ir no mercado', checked: true },
+    { task: 'Comprar sorvete', checked: false },
+  ];
 }
