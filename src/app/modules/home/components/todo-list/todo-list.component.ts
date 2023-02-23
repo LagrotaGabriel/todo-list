32bc966737c21f2289e2763d2b9f7bdc70ddcf13
Gabriel <gabriellagrota23@gmail.com>
2023-02-23 14:07:09 -0300

    feat: Ordenação de itens criada

--- src/app/modules/home/components/todo-list/todo-list.component.ts
+++ src/app/modules/home/components/todo-list/todo-list.component.ts
@@ -1,13 +1,17 @@
 import { TaskList } from './../../model/task-list';
-import { Component } from '@angular/core';
+import { Component, DoCheck } from '@angular/core';
 
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
   styleUrls: ['./todo-list.component.scss'],
 })
-export class TodoListComponent {
+export class TodoListComponent implements DoCheck{
   public taskList: Array<TaskList> = [];
+
+  ngDoCheck(): void {
+    this.taskList.sort((first, last) => Number(first.checked) - Number(last.checked));
+  }
 
   removerItem(index: number) {
     this.taskList.splice(index, 1);
